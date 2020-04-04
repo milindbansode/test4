f89f6d2cf65baee41c21826dae1c4c48719b66b0
milindbansode <milind.bansode@gmail.com>
2020-04-03 22:07:58 -0400

    Added test web application

---
+++ test4WebApp/Home.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test4WebApp
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(string.Format("test app: {0}", DateTime.Now));
+        }
+    }
+}
