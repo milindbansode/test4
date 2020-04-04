6517a5f35dedd523b8c637184d8c6b524602dc55
milindbansode <milind.bansode@gmail.com>
2020-04-03 22:40:37 -0400

    test checkin

--- test4WebApp/Home.aspx.cs
+++ test4WebApp/Home.aspx.cs
@@ -11,6 +11,7 @@ namespace test4WebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Response.Write("Hello World");
             Response.Write(string.Format("test app: {0}", DateTime.Now));
         }
     }
